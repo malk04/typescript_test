36e4a9d99e3ef64acdd160d314a4f6c2a575ea1f
malk04 <20nastya0457@gmail.com>
2025-02-12 13:47:27 +0300

    публикация проекта на github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import TablePage from "./pages/TablePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import '@blueprintjs/core/lib/css/blueprint.css';
 
 const App: React.FC = () => {
-
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<TablePage />} />
                 <Route path="*" element={<NotFoundPage />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
